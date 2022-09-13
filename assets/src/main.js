c7b1f74bddf4065e0a870ca1a2c675539744ef32
axeljk <axeljkern@yahoo.com>
2022-09-13 16:23:54 -0400

    Plane flies off the page when you scroll.

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,4 +1,9 @@
 window.onload = () => {
 	document.getElementById("plane").classList.remove("plane-off-camera");
 	document.getElementById("plane").classList.add("plane-arrive");
+
+	addEventListener("scroll", (event) => {
+		document.getElementById("plane").classList.remove("plane-arrive");
+		document.getElementById("plane").classList.add("plane-depart");
+	})
 };
